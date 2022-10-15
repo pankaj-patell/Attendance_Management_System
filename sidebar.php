8957ad45fb43db7dd15a36c359d7433393c05839
pankaj-patell <113765082+pankaj-patell@users.noreply.github.com>
2022-10-15 09:22:05 +0530

    attendance

--- sidebar.php
+++ sidebar.php
@@ -32,8 +32,7 @@
             <a href="index.php" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Dashboard
-               
+                Dashboard 
               </p>
             </a>
            
